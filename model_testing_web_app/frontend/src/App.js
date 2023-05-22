7e3d06213c0bdf0b76df63a028c8b1c26262958a
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-05-22 12:01:01 +0200

    Frontend updated

--- model_testing_web_app/frontend/src/App.js
+++ model_testing_web_app/frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/layout';
+import { Homepage, AddAirbnb } from './pages';
+
+import './App.scss';
+
+const App = () => {
+    return (
+      <>
+        <div className="app">
+          <BrowserRouter basename='/'>
+              <Routes>
+                <Route path='/' element={<Layout />} exact>
+                  <Route index element={<Homepage />} />
+                  <Route path='add-new' element={<AddAirbnb />} />
+                </Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
+    )
 }
 
-export default App;
+export default App
