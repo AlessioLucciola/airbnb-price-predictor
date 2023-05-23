b24d5be4ce3d4a3f8bcfe9665cc690f10ad59c0a
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-05-23 20:12:15 +0200

    Frontend updated

--- model_testing_web_app/frontend/src/pages/AddAirbnb/AddAirbnb.jsx
+++ model_testing_web_app/frontend/src/pages/AddAirbnb/AddAirbnb.jsx
@@ -1,63 +1,114 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+import { Popup } from '../../components';
 import './AddAirbnb.scss';
 
 function AddAirbnb() {
-    const [formValues, setFormValues] = useState({ name: '', surname: '', email: '', phone: '', request: '', captcha: '' });
-    const [focused, setFocused] = useState({ name: false, surname: false, email: false, phone: false, request: false, captcha: false });
-    const [popup, setPopup] = useState({trigger: false, title: '', description: ''});
-    const [loading, setLoading] = useState(0);
+  const [formValues, setFormValues] = useState({ name: '', address: '' });
+  const [popup, setPopup] = useState({trigger: false, title: '', description: ''});
+  const [loading, setLoading] = useState(true);
 
-    const inputs = [
-        {
-          id: 'i1',
-          name: 'name',
-          type: 'text',
-          placeholder: 'Nome*',
-          label: 'name',
-          errormessage: 'The name field can\'nt be empty!',
-          required: true,
-          focused: false
-        },
-        {
-          id: 'i2',
-          name: 'surname',
-          type: 'text',
-          placeholder: 'Cognome*',
-          label: 'surname',
-          errormessage: 'The surname field can\'t be empty',
-          required: true
-        },
-        {
-          id: 'i3',
-          name: 'email',
-          type: 'text',
-          placeholder: 'Email*',
-          label: 'email',
-          errormessage: 'Inserire un indirizzo email valido!',
-          pattern: '^[a-zA-Z0-9]+@(?:[a-zA-Z0-9.])+[.]+[?:A-Za-z]+$',
-          required: true
-        },
-        {
-          id: 'i4',
-          name: 'phone',
-          type: 'text',
-          placeholder: 'Telefono*',
-          label: 'phone',
-          errormessage: 'Inserire un numero di telefono valido!',
-          pattern: '^(?:[+]?)(?:[0-9] ?){6,14}[0-9]$',
-          required: true
-        },
-      ]
+  const inputs = {
+    name: {
+      id: 'i1',
+      name: 'name',
+      type: 'text',
+      placeholder: 'Name of the Airbnb',
+      label: 'name',
+      required: true,
+      focused: false,
+    },
+    address: {
+      id: 'i2',
+      name: 'address',
+      type: 'text',
+      placeholder: 'Address of the Airbnb',
+      label: 'address',
+      required: true,
+      focused: false,
+    }
+  }
 
-    return (
-        <div>
-            <div className='app__homepage app__container'>
-                <h1>Add a new <span className='app__homepage-title-primary-color'>AirBnb</span></h1>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    
+    let formData = {
+      name: formValues['name'],
+      surname: formValues['surname'],
+      email: formValues['email'],
+      phone: formValues['phone'],
+      request: formValues['request'],
+      captcha: formValues['captcha']
+    }
+
+    if (formValues.captcha === '') {
+      setPopup({'trigger': true, 'title': 'Si è verificato un errore!', 'description': 'Per continuare devi completare il ReCaptcha'});
+    } else {
+      axios.post('api/sendMail.php', formData)
+      .then(function (response) {
+          if (response.status === 200)
+          {
+            setPopup({'trigger': true, 'title': 'Richiesta Inviata!', 'description': response.data.message});
+            setLoading(false);
+            //removeValueFromFields();
+          } else if (response.status === 500) {
+            setPopup({'trigger': true, 'title': 'Si è verificato un errore!', 'description': response.data.message});
+            setLoading(false);
+          } else if (response.status === 401) {
+            setPopup({'trigger': true, 'title': 'Si è verificato un errore!', 'description': response.data.message});
+            setLoading(false);
+          } else {
+            setPopup({'trigger': true, 'title': 'Si è verificato un errore!', 'description': response.data.message});
+            setLoading(false);
+          }
+      })
+      .catch(function (error) {
+          setPopup({'trigger': true, 'title': 'Si è verificato un errore!', 'description': 'Si è verificato un errore interno al server. Contattaci telefonicamente per risolvere il problema.'});
+          setLoading(false);
+      });
+    }
+  }
+
+  const onChange = (e) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  };
+
+  const closePopup = () => {
+    setPopup({...popup, 'trigger': false});
+  }
+
+  return (
+    <div>
+      <div className='app__new app__container'>
+      <h1>Add a new <span className='app__homepage-title-primary-color'>AirBnb</span></h1>
+        <form className='app__new-form' onSubmit={handleSubmit}>
+          <div className='app__new-form-inputs'>
+            Give me some general information on your Airbnb!
+            <input {...inputs.name} value={formValues[inputs.name.name]} onChange={onChange} />
+            <input {...inputs.address} value={formValues[inputs.address.name]} onChange={onChange} />
+            What are your amenities?
+            <div className='app__new-form-inputs-checkboxes'>
+              <span>
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                <label>Children friendly</label>
+              </span>
+              <span>
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                <label>Pet friendly</label>
+              </span>
             </div>
-        </div>
-    )
+          </div>
+          <button type='submit'>
+            {loading ? 'Add you Airbnb' : 'Request Sent..'}
+          </button>
+        </form>
+      </div>
+
+      <Popup trigger={popup['trigger']} title={popup['title']} description={popup['description']} onClick={closePopup} />
+    </div>
+  )
 }
 
 export default AddAirbnb
