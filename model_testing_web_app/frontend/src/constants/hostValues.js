26d36e5545faf7fa16e07954841585a1050441a3
Alessio Lucciola <alessiolucciola98@hotmail.it>
2023-05-27 22:04:10 +0200

    Project updated

--- model_testing_web_app/frontend/src/constants/hostValues.js
+++ model_testing_web_app/frontend/src/constants/hostValues.js
@@ -2,10 +2,9 @@ const hostInfo = {
     is_work_email_verified: 0,
     host_identity_verified: 1,
     host_is_superhost: 1,
-    host_response_time: 0,
-    is_phone_verified: 1,
-    is_email_verified: 1
-
+    host_response_time: 1,
+    is_phone_verified: 0,
+    is_email_verified: 0
 }
 
 export default hostInfo
