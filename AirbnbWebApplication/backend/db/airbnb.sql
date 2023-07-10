e4412eaa14591abaa47739cf98ee67b737f7380d
Alessio Lucciola <alessiolucciola98@hotmail.it>
2023-07-10 17:42:01 +0200

    Database updated

--- AirbnbWebApplication/backend/db/airbnb.sql
+++ AirbnbWebApplication/backend/db/airbnb.sql
@@ -47,10 +47,9 @@ CREATE TABLE `airbnb` (
   `address` varchar(255) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   `poi_dist` double NOT NULL,
   `house_number` double NOT NULL,
+  `minimum_nights` double NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `airbnb` (`id`, `accommodates`, `bedrooms`, `n_bathrooms`, `is_children_friendly`, `availability_365`, `has_tv`, `is_bathroom_shared`, `room_type`, `has_bathtub`, `review_scores_rating`, `has_self_checkin`, `has_private_entrance`, `has_security_devices`, `calculated_host_listings_count`, `has_patio`, `is_smoking_allowed`, `city_center_dist`, `has_free_parking`, `host_identity_verified`, `station_dist`, `review_scores_cleanliness`, `host_is_superhost`, `instant_bookable`, `host_response_time`, `has_elevator`, `review_scores_location`, `has_cooking_basics`, `review_scores_value`, `number_of_reviews`, `review_scores_checkin`, `review_scores_communication`, `city`, `name`, `price`, `address`, `poi_dist`, `house_number`) VALUES
-(2,	4,	2,	2,	1,	345,	1,	0,	0,	1,	4.5,	0,	1,	1,	3,	1,	0,	0.4310380790389084,	0,	1,	0.22830711472929824,	4.8,	1,	1,	1,	1,	4.5,	1,	4.5,	15,	5,	4.5,	14,	'Bellissimo Airbnb al centro di Roma',	370,	'Piazza di spagna',	0.5473322414454348,	1);
 
--- 2023-06-01 09:50:13
+-- 2023-07-10 15:35:31
